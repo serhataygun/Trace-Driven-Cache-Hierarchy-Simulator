76cfe5417a715e41ebd608c2b7e55284ffec09d7
emretn <151366812+emretn@users.noreply.github.com.>
2026-05-05 18:47:38 +0300

    add main function

--- main.c
+++ main.c
@@ -64,3 +64,91 @@ static void usage(const char *prog)
         prog, prog, prog, prog, prog);
     exit(1);
 }
+
+int main(int argc, char *argv[])
+{
+    srand((unsigned)time(NULL));
+
+    int s = -1, E = -1, b = -1;
+    int use_l2 = 0, l2s = -1, l2E = -1, l2b = -1, exclusive = 0;
+    ReplacementPolicy rpol = POL_LRU;
+    WritePolicy       wpol = WP_WRITEBACK;
+    char *tracefile = NULL;
+
+    static struct option long_opts[] = {
+        {"l2",        no_argument,       NULL, 200},
+        {"l2-s",      required_argument, NULL, 201},
+        {"l2-E",      required_argument, NULL, 202},
+        {"l2-b",      required_argument, NULL, 203},
+        {"exclusive", no_argument,       NULL, 204},
+        {"policy",    required_argument, NULL, 210},
+        {"write",     required_argument, NULL, 211},
+        {"help",      no_argument,       NULL, 'h'},
+        {0, 0, 0, 0}
+    };
+
+    int opt, lidx;
+    while ((opt = getopt_long(argc, argv, "s:E:b:t:h", long_opts, &lidx)) != -1) {
+        switch (opt) {
+            case 's': s = atoi(optarg); break;
+            case 'E': E = atoi(optarg); break;
+            case 'b': b = atoi(optarg); break;
+            case 't': tracefile = optarg; break;
+            case 200: use_l2 = 1; break;
+            case 201: l2s = atoi(optarg); break;
+            case 202: l2E = atoi(optarg); break;
+            case 203: l2b = atoi(optarg); break;
+            case 204: exclusive = 1; break;
+            case 210:
+                if      (!strcasecmp(optarg, "FIFO"))   rpol = POL_FIFO;
+                else if (!strcasecmp(optarg, "RANDOM")) rpol = POL_RANDOM;
+                else                                    rpol = POL_LRU;
+                break;
+            case 211:
+                if (!strcasecmp(optarg, "WT")) wpol = WP_WRITETHROUGH;
+                else                           wpol = WP_WRITEBACK;
+                break;
+            case 'h': 
+            default:  usage(argv[0]);
+        }
+    }
+
+    if (s < 0 || E < 1 || b < 0 || !tracefile)
+        usage(argv[0]);
+
+    if (use_l2) {
+        if (l2s < 0) l2s = s + 2;
+        if (l2E < 0) l2E = E * 2 < 1 ? 2 : E * 2;
+        if (l2b < 0) l2b = b;
+    }
+
+    if (use_l2) {
+        long l1_cap = (1L << s) * E * (1L << b);
+        long l2_cap = (1L << l2s) * l2E * (1L << l2b);
+        if (l2_cap <= l1_cap) {
+            fprintf(stderr,
+                COL_YELLOW "Warning: L2 capacity (%ld B) ≤ L1 capacity (%ld B). "
+                "Results may be unrealistic.\n" COL_RESET, l2_cap, l1_cap);
+        }
+    }
+
+    long count = 0;
+    TraceEntry *entries = parse_trace_file(tracefile, &count);
+    printf(COL_CYAN "  Parsed %ld trace entries from '%s'\n" COL_RESET, count, tracefile);
+
+    Cache *l1 = cache_init(s, E, b, rpol, wpol, "L1");
+    Cache *l2 = use_l2 ? cache_init(l2s, l2E, l2b, rpol, wpol, "L2") : NULL;
+
+    HierarchyMode hmode = use_l2 ? (exclusive ? HIER_EXCLUSIVE : HIER_INCLUSIVE) : HIER_L1_ONLY;
+
+    Stats s1 = {0}, s2 = {0};
+    simulate(l1, l2, hmode, entries, count, &s1, &s2);
+
+    print_summary(l1, l2, hmode, rpol, wpol, tracefile, &s1, use_l2 ? &s2 : NULL, count);
+
+    free(entries);
+    cache_free(l1);
+    if (l2) cache_free(l2);
+
+    return 0;
+}
